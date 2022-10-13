9b9079240bf8ca94a5670779a5278e21a5f566f2
QueenTifah <ltfhbk@gmail.com>
2022-10-13 11:24:04 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 
 {
-	prinf("size of a char: %i byte(s)\n", sizeof(char));
-	prinf("size of a char: %i byte(s)\n", sizeof(int));
-	prinf("size of a char: %i byte(s)\n", sizeof(long int));
-	prinf("size of a char: %i byte(s)\n", sizeof(float));
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of a char: %i byte(s)\n", sizeof(int));
+	printf("size of a char: %i byte(s)\n", sizeof(long int));
+	printf("size of a char: %i byte(s)\n", sizeof(float));
 	return (0);
 }
