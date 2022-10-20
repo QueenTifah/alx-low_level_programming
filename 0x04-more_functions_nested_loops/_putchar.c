9dd41be14ab5a95704db6dfb0880dd8bcac74c78
QueenTifah <ltfhbk@gmail.com>
2022-10-20 10:25:26 -0700

    checks case

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <umistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 1 on success,-1 otherwise
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
